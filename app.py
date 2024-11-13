7ae6fd5b9ef84c497d2512f6c83fa2b00b589f65
parikshya098 <98041417+parikshya098@users.noreply.github.com>
2024-11-13 19:01:29 +0000

    Add requirements.txt

--- app.py
+++ app.py
@@ -2,24 +2,11 @@ from flask import Flask, render_template, request, redirect, url_for, flash, jso
 import requests
 from datetime import datetime
 from flask_cors import CORS
-import smtplib
-import os
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
-from sendgrid import SendGridAPIClient
-from sendgrid.helpers.mail import Mail
-from flask_mail import Mail, Message
+
 
 app = Flask(__name__)
 app.secret_key = 'saana2003'
 CORS(app, resources={r"/*": {"origins": "*"}})
-
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'your_email@gmail.com'
-app.config['MAIL_PASSWORD'] = 'your_app_password'
-mail = Mail(app)
 
 # API keys for geocoding and weather data
 GEOCODING_API_KEY = "aed05528d555dbb50a948b41b2387806"
